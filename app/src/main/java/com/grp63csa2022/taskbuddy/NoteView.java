3b8622b5ee8861ec1a4c492f3c5515bf61d7e38c
rlribaya <79568279+rlribaya@users.noreply.github.com>
2022-11-29 15:56:50 +0800

    Added Needed Activities
    NotesList
    NoteView
    TasksList
    TaskView
    SettingsPage
    DevelopersPage

---
+++ app/src/main/java/com/grp63csa2022/taskbuddy/NoteView.java
@@ -0,0 +1,14 @@
+package com.grp63csa2022.taskbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_view);
+    }
+}
