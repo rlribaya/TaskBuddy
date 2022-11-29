310593ae9cdcabb66dfa02a68946efb6e4f6203e
libanliam <90952687+libanliam@users.noreply.github.com>
2022-11-29 17:14:05 +0800

    Created Splash Screen

--- app/src/main/java/com/grp63csa2022/taskbuddy/MainActivity.java
+++ app/src/main/java/com/grp63csa2022/taskbuddy/MainActivity.java
@@ -2,7 +2,10 @@ package com.grp63csa2022.taskbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
