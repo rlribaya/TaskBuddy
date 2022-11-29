f502beaff0d3218bfa623ba2870a0c118b67c4a6
JM-Flores <juanmiguel.flores.cics@ust.edu.ph>
2022-11-29 21:48:17 +0800

    Homepage draft

--- app/src/main/java/com/grp63csa2022/taskbuddy/HomePage.java
+++ app/src/main/java/com/grp63csa2022/taskbuddy/HomePage.java
@@ -2,7 +2,10 @@ package com.grp63csa2022.taskbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Button notesButton = findViewById(R.id.notesButton);
+        Button listButton = findViewById(R.id.listButton);
+        Button settingsButton = findViewById(R.id.settingsButton);
+        Button devButton = findViewById(R.id.devButon);
+
+        notesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this,NotesList.class);
+                startActivity(intent);
+            }
+        });
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this,TasksList.class);
+                startActivity(intent);
+            }
+        });
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this,SettingsPage.class);
+                startActivity(intent);
+            }
+        });
+        devButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this,DevelopersPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
