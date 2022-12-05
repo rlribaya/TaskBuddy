99a33e9b8b8ce0fe940830f57fd5dd31522b8121
rlribaya <79568279+rlribaya@users.noreply.github.com>
2022-12-05 16:24:47 +0800

    Added Tasks

--- app/src/main/java/com/grp63csa2022/taskbuddy/TaskView.java
+++ app/src/main/java/com/grp63csa2022/taskbuddy/TaskView.java
@@ -2,13 +2,140 @@ package com.grp63csa2022.taskbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class TaskView extends AppCompatActivity {
-
+    LinearLayout task_container;
+    EditText txtTitle;
+    String[] details;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_view);
+        task_container = findViewById(R.id.task_view_tasks);
+        txtTitle = findViewById(R.id.task_view_title);
+        findViewById(R.id.task_view_add).setOnClickListener(v -> {
+            task_container.addView(
+                    createTask("", false),
+                    task_container.getChildCount()-1
+            );
+        });
+        // Check for details
+        Intent intent = getIntent();
+        details = intent.getStringArrayExtra("details"); // returns null if not found
+        if (details != null) { // IF UPDATE
+            placeTasks();
+        }
+        findViewById(R.id.task_view_save).setOnClickListener(v -> saveTasks(details != null));
+    }
+
+    private void placeTasks(){
+        /*
+            0 = id
+            1 = title
+            2 = content (each task separated by \n)
+            3 = status
+         */
+        // Place the title
+        txtTitle.setText(details[1]);
+        // Place the tasks
+        String[] task = details[2].split("\n");
+        String[] status = details[3].split("\n");
+        for (int i = 0; i < task.length; i++) {
+            System.out.println(task[i]);
+            System.out.println(status[i]);
+            task_container.addView(
+                    createTask(task[i], status[i].equals("1")),
+                    task_container.getChildCount()-1
+            );
+        }
+    }
+
+    private LinearLayout createTask(String text, Boolean checked) {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setWeightSum(1);
+        layout.setVerticalGravity(Gravity.CENTER);
+        // create the checkbox
+        CheckBox cb = new CheckBox(this);
+        cb.setChecked(checked);
+        // create the editText
+        EditText txt = new EditText(this);
+        txt.setText(text);
+        txt.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                1 // make it autofill the available space
+        ));
+        // create the delete task button
+        Button btn = new Button(this);
+        btn.setAllCaps(false);
+        btn.setText("x");
+        int btnWidth = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                40,
+                getResources().getDisplayMetrics());
+        btn.setMinHeight(0);
+        btn.setMinWidth(0);
+        btn.setPadding(0,0,0,0);
+        btn.setBackground(null);
+        btn.setLayoutParams(new ViewGroup.LayoutParams(btnWidth, ViewGroup.LayoutParams.MATCH_PARENT));
+        btn.setTextColor(Color.RED);
+        btn.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+        // delete on click
+        btn.setOnClickListener(v -> task_container.removeView(layout));
+        // add the layouts
+        layout.addView(cb);
+        layout.addView(txt);
+        layout.addView(btn);
+        return layout;
+    }
+
+    private void saveTasks(Boolean update) {
+        // Get the title
+        String title = txtTitle.getText().toString(),
+                tasks = "",
+                status = "";
+        int childCount = task_container.getChildCount()-1;
+        // Get each task and status separate using "\n" until n-1
+        for (int i = 0; i < childCount; i++) {
+            LinearLayout child = (LinearLayout) task_container.getChildAt(i);
+            /*
+                0 = checkbox
+                1 = edit text
+                2 = delete button
+             */
+            CheckBox child_status = (CheckBox) child.getChildAt(0);
+            EditText child_task = (EditText) child.getChildAt(1);
+            tasks += child_task.getText().toString() + "\n";
+            status += (child_status.isChecked() ? "1" : "0") + "\n";
+        }
+
+        // Get now the Database
+        DBHandler db = new DBHandler(this);
+        if (update){
+            db.updateTask(details[0], title, tasks, status);
+            Toast.makeText(this, "Updated Task Successfully", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            db.insertTask(title, tasks, status);
+            Toast.makeText(this, "Saved Task Successfully", Toast.LENGTH_SHORT).show();
+        }
+
+        finish();
     }
 }
--- app/src/main/java/com/grp63csa2022/taskbuddy/TasksList.java
+++ app/src/main/java/com/grp63csa2022/taskbuddy/TasksList.java
@@ -2,7 +2,15 @@ package com.grp63csa2022.taskbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class TasksList extends AppCompatActivity {
 
@@ -10,5 +18,40 @@ public class TasksList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks_list);
+        findViewById(R.id.tasks_add).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), TaskView.class)));
+        generateTasksList();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        generateTasksList();
+    }
+
+    private void generateTasksList() {
+        DBHandler db = new DBHandler(getApplicationContext());
+        GridView gridView = findViewById(R.id.tasks_list);
+        ListAdapter adapter = new SimpleAdapter(
+                getApplicationContext(),
+                db.getAllTasks(),
+                R.layout.list_row,
+                db.getTasksColumnNames(),
+                new int[]{R.id.row_id, R.id.row_title, R.id.row_content, R.id.row_status}
+        );
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener((adapterView, view, i, l) -> {
+            LinearLayout linearLayout = (LinearLayout) view;
+
+            int max = linearLayout.getChildCount();
+            String[] details = new String[max];
+            while (max-- > 0) {
+                TextView textView = (TextView) linearLayout.getChildAt(max);
+                details[max] = textView.getText().toString();
+            }
+            Intent intent = new Intent(getApplicationContext(), TaskView.class);
+            intent.putExtra("details", details);
+            System.out.println(Arrays.toString(details));
+            startActivity(intent);
+        });
     }
 }
