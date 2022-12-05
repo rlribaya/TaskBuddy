6c60d5256c9d615170206319645287d5db96f8c5
rlribaya <79568279+rlribaya@users.noreply.github.com>
2022-12-05 16:24:55 +0800

    Added Notes

--- app/src/main/java/com/grp63csa2022/taskbuddy/NoteView.java
+++ app/src/main/java/com/grp63csa2022/taskbuddy/NoteView.java
@@ -2,7 +2,13 @@ package com.grp63csa2022.taskbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class NoteView extends AppCompatActivity {
 
@@ -10,5 +16,47 @@ public class NoteView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_view);
+
+        EditText txtTitle = findViewById(R.id.note_view_title),
+                txtContent = findViewById(R.id.note_view_content);
+
+        Intent intent = getIntent();
+        String[] details = intent.getStringArrayExtra("details");
+        boolean update = details != null;
+        if (update) {
+            txtTitle.setText(details[1]);
+            txtContent.setText(details[2]);
+
+            LinearLayout layout = findViewById(R.id.note_main);
+            Button btn_delete = new Button(getApplicationContext());
+            DBHandler db = new DBHandler(getApplicationContext());
+
+            btn_delete.setText("Delete");
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            layoutParams.gravity = Gravity.CENTER;
+            btn_delete.setLayoutParams(layoutParams);
+            btn_delete.setOnClickListener(v->{
+                db.deleteNote(details[0]);
+                Toast.makeText(this, "Successfully Deleted Note", Toast.LENGTH_SHORT).show();
+                finish();
+            });
+            layout.addView(btn_delete);
+        }
+        findViewById(R.id.note_view_save).setOnClickListener(v -> {
+            String title = txtTitle.getText().toString();
+            String content = txtContent.getText().toString();
+
+            DBHandler db = new DBHandler(getApplicationContext());
+            if (update) {
+                db.updateNote(details[0], title, content);
+            } else {
+                db.insertNote(title, content);
+            }
+            Toast.makeText(getApplicationContext(), "Saved Successfully", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 }
--- app/src/main/java/com/grp63csa2022/taskbuddy/NotesList.java
+++ app/src/main/java/com/grp63csa2022/taskbuddy/NotesList.java
@@ -2,7 +2,17 @@ package com.grp63csa2022.taskbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class NotesList extends AppCompatActivity {
 
@@ -10,5 +20,41 @@ public class NotesList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        generateNotesList();
+        findViewById(R.id.notes_add).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), NoteView.class)));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        generateNotesList();
+    }
+    private void generateNotesList() {
+        DBHandler db = new DBHandler(getApplicationContext());
+        ListView listView = findViewById(R.id.notes_list);
+
+        ListAdapter adapter = new SimpleAdapter(
+                getApplicationContext(),
+                db.getAllNotes(),
+                R.layout.list_row,
+                db.getNotesColumnNames(),
+                new int[]{R.id.row_id, R.id.row_title, R.id.row_content}
+        );
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((adapterView, view, i, l) -> listOnClick(view));
+    }
+    private void listOnClick(View view) {
+        LinearLayout linearLayout = (LinearLayout) view;
+
+        int max = linearLayout.getChildCount();
+        String[] details = new String[max];
+        while (max-- > 0) {
+            TextView textView = (TextView) linearLayout.getChildAt(max);
+            details[max] = textView.getText().toString();
+        }
+        Intent intent = new Intent(getApplicationContext(), NoteView.class);
+        intent.putExtra("details", details);
+        System.out.println(Arrays.toString(details));
+        startActivity(intent);
     }
 }
